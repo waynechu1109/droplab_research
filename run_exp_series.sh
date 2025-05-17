f2129fc88aa89978bfff50c88fa4da0ada4a316d
waynechu1109 <waynechu1109@gmail.com>
2025-05-17 16:45:54 +0800

    add: requirements.txt update

--- run_exp_series.sh
+++ run_exp_series.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+export OPENBLAS_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+
 ./experiment.sh NeuS_pe6_beta100_4.2_.5_.01to.05_[]_.05_1_adamw_cosAnn_narrow_band_msk0.05 2500 0.005 0.01 1.0
 # ./experiment.sh NeuS_pe6_beta100_4.2_.5_.01to.05_[]_.05_.5_adamw_cosAnn_narrow_band_msk0.05 2500 0.005 0.01 0.5
 # ./experiment.sh NeuS_pe6_beta100_4.2_.5_.01to.05_[]_.05_.1_adamw_cosAnn_narrow_band_msk0.05 2500 0.005 0.01 0.1
