198d9cc1a1eba2a732f3672039f525afb3888a2c
waynechu1109 <waynechu1109@gmail.com>
2025-06-03 06:05:19 +0800

    exp: two-step training exp 7 (A100) (PE with render loss implement more correctly)

--- scripts/run_exp_series.sh
+++ scripts/run_exp_series.sh
@@ -27,11 +27,11 @@ export OMP_NUM_THREADS=1
 # done
 
 
-for file in dtu_73_42 dtu_82_14 dtu_90_41 dtu_110_43 dtu_114_32 dtu shoes; do
+for file in dtu_65_47 dtu_73_42 dtu_82_14 dtu_90_41 dtu_110_43 dtu_114_32 dtu shoes; do
     for lr in 0.0055; do
         for loss_render in 10.0; do
-            echo -e "\033[34m$file: pe_normalized_3rgb_render1.0~${loss_render}_${lr}\033[0m"
-            ./scripts/experiment.sh "pe_normalized_3rgb_render1.0~${loss_render}_${lr}" "$lr" "$file" schedule false $loss_render
+            echo -e "\033[34m$file: pe_normalized_3rgb_a100_render1.0~${loss_render}_${lr}\033[0m"
+            ./scripts/experiment.sh "pe_normalized_3rgb_a100_render1.0~${loss_render}_${lr}" "$lr" "$file" schedule_a100 true $loss_render
         done
     done
 done
